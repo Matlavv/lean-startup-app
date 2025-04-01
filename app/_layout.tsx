804ae03b1007ea0c52db11611b4750c2fdc2a7e3
Alexis <90972106+alexishadjian@users.noreply.github.com>
2025-04-01 21:02:26 +0200

    feat: clean default files, add colors variables

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,13 +11,11 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 import '../global.css';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -33,12 +31,12 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'light' ? DarkTheme : DefaultTheme}>
+    <>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </>
   );
 }
---
+++ styles/Colors.ts
@@ -0,0 +1,9 @@
+const black = '#000';
+const white = '#fff';
+const pink = '#FF0381';
+
+export const Colors = {
+  background: white,
+  text: black,
+  primary: pink,
+};
